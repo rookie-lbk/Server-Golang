df524b1812c51ef18ae1999104cec42442aec3b5
liubenkang <benkang.liu@centurygame.com>
2025-12-04 00:00:42 +0800

    no message

--- main.go
+++ main.go
@@ -12,51 +12,24 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	// 将 "/" 路径映射到文件服务器
 	http.Handle("/", fileServer)
-	// 定义 "/hello" 路径的处理器
-	http.HandleFunc("/hello", HelloHandler)
-	// 定义 "/form" 路径的处理器
-	http.HandleFunc("/form", FormHandler)
+	// 定义 "/privacy-policy" 路径的处理器（隐私协议）
+	http.HandleFunc("/privacy-policy", PrivacyPolicyHandler)
+	// 定义 "/terms-of-service" 路径的处理器（使用条款）
+	http.HandleFunc("/terms-of-service", TermsOfServiceHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	// 启动HTTP服务器并监听端口 8080，如果出现错误，则打印错误信息并退出
-	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	//判断请求路径是否正确
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-	//判断请求方式是否正确
-	if r.Method != "GET" {
-		http.Error(w, "method is not supported", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintf(w, "hello!")
+// PrivacyPolicyHandler 隐私协议处理器
+func PrivacyPolicyHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/privacy-policy.html")
 }
 
-func FormHandler(w http.ResponseWriter, r *http.Request) {
-	//判断请求路径是否正确
-	if r.URL.Path != "/form" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-	//判断请求方式是否正确
-	if r.Method != "POST" {
-		http.Error(w, "method is not supported", http.StatusNotFound)
-		return
-	}
-	//解析表单数据是否正确
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "POST request successful\n")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "name is:%s\n", name)
-	fmt.Fprintf(w, "address is: %s", address)
+// TermsOfServiceHandler 使用条款处理器
+func TermsOfServiceHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/terms-of-service.html")
 }
